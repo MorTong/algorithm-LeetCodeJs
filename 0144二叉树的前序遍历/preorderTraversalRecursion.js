e0f7b6f2fbd706345764cbc3085a82916354b21e
unknown <1481224733@qq.com>
2018-09-10 22:39:09 +0800

    前序遍历递归实现

---
+++ 0144二叉树的前序遍历/preorderTraversalRecursion.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root,res=[]) {
+  if(root){
+      res.push(root.val);
+      preorderTraversal(root.left,res);
+      preorderTraversal(root.right,res);
+  }
+  return res;
+};
